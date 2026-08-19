9e88bd9a3c1ecbde74ca642ac34ef21c81d93e97
Fang <fahdionsanon@gmail.com>
2026-08-19 10:40:18 +0000

    ajout de incident

---
+++ src/main/java/com/Spring/Cicada/model/Incident.java
@@ -0,0 +1,48 @@
+package com.Spring.Cicada.model;
+
+import jakarta.persistence.*;
+@Entity
+
+public class Incident {
+     @Id
+     @GeneratedValue
+     
+    
+        private Long id;
+        private String titre;
+        private String descriptions;
+        private String statut;
+
+        public Incident() {
+        
+    }
+        public Long getId() {
+            return id;
+        }
+        public void setId(Long id) {
+            this.id = id;
+        }
+        public String getTitre() {
+            return titre;
+        }
+        public void setTitre(String titre) {
+            this.titre = titre;
+        }
+        public String getDescriptions() {
+            return descriptions;
+        }
+        public void setDescriptions(String descriptions) {
+            this.descriptions = descriptions;
+        }
+        public String getStatut() {
+            return statut;
+        }
+        public void setStatut(String statut) {
+            this.statut = statut;
+        }
+
+
+
+
+    
+}
