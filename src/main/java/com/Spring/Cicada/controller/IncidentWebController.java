efd9a4e823f05e7073ec60045c7d5b2f1b1f6481
Fang <fahdionsanon@gmail.com>
2026-08-21 11:37:16 +0000

    Ajout d'une interface

--- src/main/java/com/Spring/Cicada/controller/IncidentWebController.java
+++ src/main/java/com/Spring/Cicada/controller/IncidentWebController.java
@@ -1,10 +1,12 @@
 package com.Spring.Cicada.controller;
 
+import com.Spring.Cicada.model.Incident;
 import com.Spring.Cicada.service.IncidentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class IncidentWebController {
@@ -16,5 +18,17 @@ public class IncidentWebController {
     public String listeIncidents(Model model) {
         model.addAttribute("incidents", incidentService.getAllIncidents());
         return "liste-incidents";
+    }
+
+    @GetMapping("/incidents/Creation de Formulaire")
+    public String formulaireIncident(Model model) {
+        model.addAttribute("incident", new Incident());
+        return "formulaire-incident";
+    }
+
+    @PostMapping("/incidents/Creation de Formulaire")
+    public String creerIncident(Incident incident) {
+        incidentService.createIncident(incident);
+        return "redirect:/incidents";
     }
 }
