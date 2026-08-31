abb8eb27a26209be7ad830c6cb94e5d5bfa05534
Fang <fahdionsanon@gmail.com>
2026-08-31 12:19:32 +0000

    ajout de solutions

---
+++ src/main/java/com/Spring/Cicada/controller/ProblemeWebController.java
@@ -0,0 +1,34 @@
+package com.Spring.Cicada.controller;
+
+import com.Spring.Cicada.model.Probleme;
+import com.Spring.Cicada.service.ProblemeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ProblemeWebController {
+
+    @Autowired
+    private ProblemeService problemeService;
+
+    @GetMapping("/problemes")
+    public String listeProblemes(Model model) {
+        model.addAttribute("problemes", problemeService.getAllProblemes());
+        return "liste-problemes";
+    }
+
+    @GetMapping("/problemes/nouveau")
+    public String formulaireProbleme(Model model) {
+        model.addAttribute("probleme", new Probleme());
+        return "formulaire-probleme";
+    }
+
+    @PostMapping("/problemes/nouveau")
+    public String creerProbleme(Probleme probleme) {
+        problemeService.createProbleme(probleme);
+        return "redirect:/problemes";
+    }
+}
